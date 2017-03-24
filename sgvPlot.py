d5f6e4e78771e1bdd265a1561b4266312b90fa28
mmosmond <mmosmond@gmail.com>
2017-03-24 15:02:23 -0700

    plot colors for each gen

--- sgvPlot.py
+++ sgvPlot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+import matplotlib.cm as cm
 
 K = 1000 #max number of parents (positive integer)
 n = 2 #number of traits (positive integer)
@@ -10,9 +11,10 @@ alpha = 0.1 #mu
 
 sim_id = 'K%d_n%d_B%d_u%r_alpha%r' %(K,n,B,u,alpha)
 
+data_dir = 'data'
 
 # load pop data
-f = open('pop_%s.pkl' %(sim_id), 'rb')
+f = open('%s/pop_%s.pkl' %(data_dir,sim_id), 'rb')
 pop = []
 while 1:
     try:
@@ -21,7 +23,7 @@ while 1:
         break
 
 # load mut data
-g = open('mut_%s.pkl' %(sim_id), 'rb')
+g = open(('%s/mut_%s.pkl' %(data_dir,sim_id), 'rb')
 mut = []
 while 1:
     try:
@@ -30,7 +32,7 @@ while 1:
         break
 
 # load gen data
-f = open('gen_%s.pkl' %(sim_id), 'rb')
+f = open('%s/gen_%s.pkl' %(data_dir,sim_id), 'rb')
 gen = []
 while 1:
     try:
@@ -40,18 +42,15 @@ while 1:
 
 # make phenotype data
 phenos = []
-i = 0
-while i < len(gen): 
+for i in range(len(gen)):
 	phenos.append(np.dot(pop[i],mut[i]))
-	i = i + 1
 
 
 # plot phenotypes
-i = 0
-while i < len(phenos): 
-	plt.scatter(phenos[i][:,0],phenos[i][:,1])
-	plt.show()
-	i = i + 1
+colors = cm.rainbow(np.linspace(0, 1, len(phenos)))
+for i, c in zip(range(len(phenos)), colors):
+    plt.scatter(phenos[i][:,0],phenos[i][:,1], color=c)
+
 plt.show()
 
 
