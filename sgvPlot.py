7904ae204b1062b961d6bb84bab79c66399239a9
mmosmond <mmosmond@gmail.com>
2017-03-24 17:19:33 -0700

    sims adapting to new optimum, can plot, can convert pickle data to csv for R

--- sgvPlot.py
+++ sgvPlot.py
@@ -7,7 +7,7 @@ K = 1000 #max number of parents (positive integer)
 n = 2 #number of traits (positive integer)
 B = 2 #number of offspring per generation per parent (positive integer)
 u = 0.001 #mutation probability per genome (0<u<1)
-alpha = 0.1 #mu
+alpha = 0.01 #mutation SD
 
 sim_id = 'K%d_n%d_B%d_u%r_alpha%r' %(K,n,B,u,alpha)
 
@@ -23,7 +23,7 @@ while 1:
         break
 
 # load mut data
-g = open(('%s/mut_%s.pkl' %(data_dir,sim_id), 'rb')
+g = open('%s/mut_%s.pkl' %(data_dir,sim_id), 'rb')
 mut = []
 while 1:
     try:
@@ -45,13 +45,18 @@ phenos = []
 for i in range(len(gen)):
 	phenos.append(np.dot(pop[i],mut[i]))
 
+# save pheno data as CSV for R (can take a little time!)
+# import csv
+# with open("%s/phenos_%s.csv" %(data_dir,sim_id), "w") as f:
+#     writer = csv.writer(f)
+#     writer.writerows(phenos)
 
 # plot phenotypes
 colors = cm.rainbow(np.linspace(0, 1, len(phenos)))
 for i, c in zip(range(len(phenos)), colors):
     plt.scatter(phenos[i][:,0],phenos[i][:,1], color=c)
-
-plt.show()
+# plt.show()
+plt.savefig('%s/plot_%s.png' %(data_dir,sim_id))
 
 
 
