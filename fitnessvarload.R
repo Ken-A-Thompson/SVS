246225e822d16dff34296891034ef9ebab3e0356
mmosmond <mmosmond@gmail.com>
2018-06-15 17:23:45 -0700

    fixed error in varfit r code, but dont know how to save rows to dataframe or plot

--- fitnessvarload.R
+++ fitnessvarload.R
@@ -1,10 +1,10 @@
-install.packages("R2Cuba")
-library("R2Cuba")
+#install.packages("R2Cuba")
+library("R2Cuba") #for integrating multivariate functions
 
-install.packages("mvtnorm")
-library("mvtnorm")
+#install.packages("mvtnorm")
+library("mvtnorm") #for multivariate normal function
 
-
+#this is what we want to integrate, over traits 1 and 2, for a given angle between parental optima (theta) and phenotypic variance in the hybirds (lambda)
 integrand <- function(arg, theta, lambda) {
   x <- arg[1] #trait 1
   y <- arg[2] #trait 2
@@ -12,23 +12,32 @@ integrand <- function(arg, theta, lambda) {
   y1 <- 0 #optimum y in enviro 1
   x2 <- cos(theta*pi/180) #optimum x in enviro 2
   y2 <- sin(theta*pi/180) #optimum x in enviro 2
-  pxy <- dmvnorm( arg, mean = c((x1+x2)/2, (y1+y2)/2), sigma = lambda*diag(2), log=FALSE ) #distribution of traits (normal with mean between optima and variance lambda)
-  wxy1 <- 2*pi*dmvnorm( arg, mean = c(x1, y2), sigma = diag(2), log=FALSE ) #fitness in enviro 1
-  wxy2 <- 2*pi*dmvnorm( arg, mean = c(x1, y2), sigma = diag(2), log=FALSE ) #fitness in enviro 2
+  pxy <- dmvnorm( arg, mean = c((x1+x2)/2, (y1+y2)/2), sigma = lambda*diag(2), log=FALSE ) #distribution of traits (multivariate normal with mean exactly between parental optima and variance lambda in each direction, no covariance)
+  wxy1 <- 2*pi*dmvnorm( arg, mean = c(x1, y1), sigma = diag(2), log=FALSE ) #fitness in enviro 1
+  wxy2 <- 2*pi*dmvnorm( arg, mean = c(x2, y2), sigma = diag(2), log=FALSE ) #fitness in enviro 2
   novar <- 2*pi*dmvnorm( c((x1+x2)/2, (y1+y2)/2), mean = c(x1, y2), sigma = diag(2), log=FALSE ) #mean fitness if no variation
   ff <- max(wxy1,wxy2) * pxy / novar; #max fitness times density divided by no variance fitness (effect of variance on mean fitness)
   return(ff)
-} # end integrand
+}
 
+#input lambda and theta and get back theta, variance load, and relative mean fitness of hybrids
 points <- function(lambda, theta){
   varload <- -log(1/(1+lambda)) #variance load
   deltaw <- cuhre(ndim=2, ncomp=1, integrand, theta=theta, lambda=lambda, lower=rep(-100,2), upper=rep(100,2), rel.tol=1e-3, abs.tol=1e-12, flags=list(verbose=0,final=1)) #effect on mean fitness
   return(c(theta,varload,deltaw$value)) #return angle, variance load, and effect of variance on mean fitness
 }
 
+#run this over a whole bunch of thetas and lambdas to get data to plot
+datalist <- list()
+i=1
 for(theta in c(0,60,180)){
   for(lambda in c(seq(0.0001, 0.12, 0.01))){
-    print(points(lambda=lambda, theta=theta))
+    
+    datalist[[i]] <- points(lambda=lambda, theta=theta)
+    
+    i <- i + 1          
   }
 }
+ 
+data
 
